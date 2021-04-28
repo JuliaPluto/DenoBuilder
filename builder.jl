7605e3e8573e0ff47776c7d923b5abce01aaf9d4
Paul Berg <paul.berg@etu.utc.fr>
2021-04-28 21:46:49 +0200

    fix builder script

--- builder.jl
+++ builder.jl
@@ -2,7 +2,7 @@ using Pkg.Artifacts
 using Pkg.BinaryPlatforms
 
 deno_version = v"1.9.2"
-build_number = 2
+build_number = 3
 
 # Where the GitHub release files will be located
 build_path = joinpath(pwd(), "build/")
@@ -11,7 +11,7 @@ build_path = joinpath(pwd(), "build/")
 artifacts_toml = joinpath(build_path, "Artifacts.toml")
 
 # Clean the build folder
-if ispath(build_path)
+if isdir(build_path)
   rm(build_path; force=true, recursive=true)
 end
 mkdir(build_path)
@@ -38,17 +38,22 @@ mktempdir() do temp_path
 
     zip_name = split(archive_url, "/")[end]
     archive_name = download(archive_url, joinpath(temp_path, zip_name))
-    dest_dir = replace(basename(archive_name), ".zip" => "")
+    dest_dir_name = replace(basename(archive_name), ".zip" => "")
+    dest_dir = joinpath(temp_path, dest_dir_name)
     run(`unzip $(archive_name) -d $(dest_dir)`)
 
     exe_name = platform isa Windows ? "deno.exe" : "deno"
     artifact_hash = create_artifact() do artifact_dir
-      mv(joinpath(dest_dir, exe_name), joinpath(artifact_dir, exe_name))
+      src = joinpath(dest_dir, exe_name)
+      dest = joinpath(artifact_dir, exe_name)
+      @info "$src -> $dest"
+      mv(src, dest)
     end
 
-    archive_name = "$dest_dir-$deno_version-$build_number.tar.gz"
+    archive_name = "$dest_dir_name-$deno_version-$build_number.tar.gz"
     archive_dest = joinpath(build_path, archive_name)
     download_hash = archive_artifact(artifact_hash, archive_dest)
+
     github_url = "https://github.com/Pangoraw/Deno.jl/releases/download/v$deno_version-$build_number/$archive_name"
     bind_artifact!(artifacts_toml, "deno_exe", artifact_hash; platform=platform, force=true, download_info=[
       (github_url, download_hash)
